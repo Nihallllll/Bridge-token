5e78ccadb8ffc272c91740e8975a5d25163071e8
Nihallllll <nihal24rajak@gmail.com>
2025-08-01 18:54:41 +0530

    first commit

--- contract/test/Counter.t.sol
+++ contract/test/Counter.t.sol
@@ -5,20 +5,20 @@ import {Test, console} from "forge-std/Test.sol";
 import {BridgeContract} from "../src/Bridge_contract.sol";
 
 contract CounterTest is Test {
-    BridgeContract public bridgeContract;
+    // BridgeContract public bridgeContract;
 
-    function setUp() public {
-        bridgeContract = new BridgeContract();
-        bridgeContract.setNumber(0);
-    }
+    // function setUp() public {
+    //     bridgeContract = new BridgeContract();
+    //     bridgeContract.setNumber(0);
+    // }
 
-    function test_Increment() public {
-        bridgeContract.increment();
-        assertEq(bridgeContract.number(), 1);
-    }
+    // function test_Increment() public {
+    //     bridgeContract.increment();
+    //     assertEq(bridgeContract.number(), 1);
+    // }
 
-    function testFuzz_SetNumber(uint256 x) public {
-        bridgeContract.setNumber(x);
-        assertEq(bridgeContract.number(), x);
-    }
+    // function testFuzz_SetNumber(uint256 x) public {
+    //     bridgeContract.setNumber(x);
+    //     assertEq(bridgeContract.number(), x);
+    // }
 }
